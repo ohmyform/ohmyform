af22df803ce07e92dfe0c7c165213339afde391e
Suzanne Aitchison <suzanne.aitchison@gmail.com>
2019-10-12 12:30:26 +0100

    make the footer a footer

--- ui/layouts/screen.vue
+++ ui/layouts/screen.vue
@@ -4,12 +4,12 @@
       <nuxt />
     </main>
 
-    <div class="footer">
+    <footer class="footer">
       <nuxt-link to="/login">Login</nuxt-link>
       <nuxt-link to="/register">Register</nuxt-link>
       <nuxt-link to="/admin">Manage</nuxt-link>
       <a href="https://ohmyform.com">OhMyForm</a>
-    </div>
+    </footer>
   </div>
 </template>
 
