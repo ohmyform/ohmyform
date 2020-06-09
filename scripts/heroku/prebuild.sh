b3d7f26b41aa7d4b93e038ca431f7a814c9b545c
Michael Schramm <michael.schramm@gmail.com>
2020-06-09 17:19:25 +0200

    show current ref in heroku

--- scripts/heroku/prebuild.sh
+++ scripts/heroku/prebuild.sh
@@ -3,6 +3,11 @@ echo "Fetch Submodules"
 git clone --depth 1 https://github.com/ohmyform/ui ui
 git clone --depth 1 https://github.com/ohmyform/api api
 
+echo "UI ref is"
+git --git-dir=ui/.git rev-parse HEAD
+echo "API ref is"
+git --git-dir=api/.git rev-parse HEAD
+
 echo "Install UI"
 yarn --cwd ui install --frozen-lockfile
 
